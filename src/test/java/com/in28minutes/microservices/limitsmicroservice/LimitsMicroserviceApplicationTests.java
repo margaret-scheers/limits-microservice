0b8d23d49b914f5a485c6396ee2d4e503ac5d99a
Margaret Scheers <margaret.scheers@gfs.com>
2021-03-23 09:32:10 -0500

    Initial Commit

---
+++ src/test/java/com/in28minutes/microservices/limitsmicroservice/LimitsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.limitsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
