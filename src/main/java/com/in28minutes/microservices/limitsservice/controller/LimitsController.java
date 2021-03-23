63617b7fedd592a886e4ce7d1d655ed8590c9c51
Margaret Scheers <margaret.scheers@gfs.com>
2021-03-23 12:49:40 -0500

    read the limits from a configuration

--- src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsController.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsController.java
@@ -1,14 +1,25 @@
 package com.in28minutes.microservices.limitsservice.controller;
 
 import com.in28minutes.microservices.limitsservice.bean.Limits;
+import com.in28minutes.microservices.limitsservice.configuration.LimitsConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class LimitsController {
+
+
+    @Autowired
+    private LimitsConfiguration configuration;
+
     @GetMapping("/limits")
     public Limits retrieveLimits() {
-        return new Limits(1,1000);
+        return new Limits(configuration.getMinimum(),
+                configuration.getMaximum());
+        //return new Limits(1,1000);
 
     }
 }
